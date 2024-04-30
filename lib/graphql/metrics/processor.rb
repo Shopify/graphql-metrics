9a59d82aef483ecfe58528fcfb8f4707d0208181
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2024-04-30 15:32:00 -0400

    Re-add interrupted logic

--- lib/graphql/metrics/processor.rb
+++ lib/graphql/metrics/processor.rb
@@ -7,19 +7,19 @@ module GraphQL
       def initialize(query_or_multiplex, **options)
       end
 
-      def field_extracted(field_metric)
+      def field_extracted(field_metric, query:)
         raise NotImplementedError
       end
 
-      def query_extracted(query_metrics)
+      def query_extracted(query_metrics, query:)
         raise NotImplementedError
       end
 
-      def argument_extracted(argument_metric)
+      def argument_extracted(argument_metric, query:)
         raise NotImplementedError
       end
 
-      def directive_extracted(directive_metric)
+      def directive_extracted(directive_metric, query:)
         raise NotImplementedError
       end
     end
